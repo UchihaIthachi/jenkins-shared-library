bb1bc513109ba567ef99a9fff26cf6259e92a6d1
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-09-15 04:47:10 +0530

    update

--- src/com/docker/Docker.groovy
+++ src/com/docker/Docker.groovy
@@ -2,6 +2,7 @@
 
 package com.docker
 
+// Implements serializable interface because we need to save the state of the execution if the pipeline is paused / resumed
 class Docker implements Serializable {
 
     def script
@@ -10,23 +11,17 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def isWindows() {
-        return env.OS?.toLowerCase().contains("windows")
-    }
-
     def buildDockerImage(String imageName) {
+
         script.echo "Building docker image"
 
-        if (isWindows()) {
-            // Windows specific command
-            script.bat "docker build -t ${imageName} ."
-        } else {
-            // Linux / Unix specific command
-            script.sh "docker build -t ${imageName} ."
-        }
+        // Execute a shell command to build a Docker image
+        script.bat "docker build -t $imageName ."
+
     }
 
     def dockerHubLogin() {
+
         script.echo "Logging in to Docker Hub"
 
         script.withCredentials([script.usernamePassword(
@@ -34,25 +29,21 @@ class Docker implements Serializable {
                 passwordVariable: 'DOCKER_HUB_PASSWORD',
                 usernameVariable: 'DOCKER_HUB_USERNAME')]) {
 
-            if (isWindows()) {
-                // Windows specific command
-                script.bat "echo ${script.DOCKER_HUB_PASSWORD} | docker login -u ${script.DOCKER_HUB_USERNAME} --password-stdin"
-            } else {
-                // Linux / Unix specific command
-                script.sh "echo '${script.DOCKER_HUB_PASSWORD}' | docker login -u '${script.DOCKER_HUB_USERNAME}' --password-stdin"
-            }
+            // Execute a shell command to log in to Docker Hub
+            // The `echo $DOCKER_HUB_PASSWORD` part is used to pass the password to the `docker login` command
+            // The `--password-stdin` option tells Docker to read the password from the standard input
+            script.bat "echo '${script.DOCKER_HUB_PASSWORD}' | docker login -u '${script.DOCKER_HUB_USERNAME}' --password-stdin"
+
+
         }
     }
 
     def dockerHubPush(String imageName) {
+
         script.echo "Pushing docker image to Docker Hub"
 
-        if (isWindows()) {
-            // Windows specific command
-            script.bat "docker push ${imageName}"
-        } else {
-            // Linux / Unix specific command
-            script.sh "docker push ${imageName}"
-        }
+        // Execute a shell command to push the Docker image to Docker Hub
+        script.bat "docker push $imageName"
     }
+
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,17 +1,6 @@
 #!/usr/bin/env groovy
 
-def isWindows() {
-    return env.OS?.toLowerCase().contains("windows")
-}
-
 def call() {
-    echo "Building jar artifact for $GIT_BRANCH" // Reference to the environment variable
-
-    if (isWindows()) {
-        // Windows-specific command
-        bat "mvn clean package"
-    } else {
-        // Linux/Unix-specific command
-        sh "mvn clean package"
-    }
+    echo "Building jar artifact for $GIT_BRANCH" // GIT_BRANCH - reference to the environment variable
+    bat "mvn clean package"
 }
