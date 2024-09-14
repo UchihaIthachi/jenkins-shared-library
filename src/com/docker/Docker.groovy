cf7d23146449f54ac31006734ebf928e39f89c3b
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-09-15 04:17:26 +0530

    add os detect

--- src/com/docker/Docker.groovy
+++ src/com/docker/Docker.groovy
@@ -2,7 +2,6 @@
 
 package com.docker
 
-// Implements serializable interface because we need to save the state of the execution if the pipeline is paused / resumed
 class Docker implements Serializable {
 
     def script
@@ -11,17 +10,23 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def buildDockerImage(String imageName) {
+    def isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win")
+    }
 
+    def buildDockerImage(String imageName) {
         script.echo "Building docker image"
 
-        // Execute a shell command to build a Docker image
-        script.sh "docker build -t $imageName ."
-
+        if (isWindows()) {
+            // Windows specific command
+            script.bat "docker build -t ${imageName} ."
+        } else {
+            // Linux / Unix specific command
+            script.sh "docker build -t ${imageName} ."
+        }
     }
 
     def dockerHubLogin() {
-
         script.echo "Logging in to Docker Hub"
 
         script.withCredentials([script.usernamePassword(
@@ -29,21 +34,25 @@ class Docker implements Serializable {
                 passwordVariable: 'DOCKER_HUB_PASSWORD',
                 usernameVariable: 'DOCKER_HUB_USERNAME')]) {
 
-            // Execute a shell command to log in to Docker Hub
-            // The `echo $DOCKER_HUB_PASSWORD` part is used to pass the password to the `docker login` command
-            // The `--password-stdin` option tells Docker to read the password from the standard input
-            script.sh "echo '${script.DOCKER_HUB_PASSWORD}' | docker login -u '${script.DOCKER_HUB_USERNAME}' --password-stdin"
-
-
+            if (isWindows()) {
+                // Windows specific command
+                script.bat "echo ${script.DOCKER_HUB_PASSWORD} | docker login -u ${script.DOCKER_HUB_USERNAME} --password-stdin"
+            } else {
+                // Linux / Unix specific command
+                script.sh "echo '${script.DOCKER_HUB_PASSWORD}' | docker login -u '${script.DOCKER_HUB_USERNAME}' --password-stdin"
+            }
         }
     }
 
     def dockerHubPush(String imageName) {
-
         script.echo "Pushing docker image to Docker Hub"
 
-        // Execute a shell command to push the Docker image to Docker Hub
-        script.sh "docker push $imageName"
+        if (isWindows()) {
+            // Windows specific command
+            script.bat "docker push ${imageName}"
+        } else {
+            // Linux / Unix specific command
+            script.sh "docker push ${imageName}"
+        }
     }
-
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,17 @@
 #!/usr/bin/env groovy
 
+def isWindows() {
+    return System.getProperty("os.name").toLowerCase().contains("win")
+}
+
 def call() {
-    echo "Building jar artifact for $GIT_BRANCH" // GIT_BRANCH - reference to the environment variable
-    sh "mvn clean package"
+    echo "Building jar artifact for $GIT_BRANCH" // Reference to the environment variable
+
+    if (isWindows()) {
+        // Windows-specific command
+        bat "mvn clean package"
+    } else {
+        // Linux/Unix-specific command
+        sh "mvn clean package"
+    }
 }
